b5b5c73134754746140fb25d2b1ddd247f42caaf
Keiron-Beadle <K.BEADLE-2018@hull.ac.uk>
2020-12-19 07:45:10 +0000

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform playerTransform;
+    public PlayerController playerController;
+
+    float xRot = 0f;
+    public float mouseSensitivity = 100f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        bool firstPerson = playerController.firstPerson;
+        if (firstPerson)
+        {
+            FirstPersonUpdate();
+        }
+        else
+        {
+            ThirdPersonUpdate();
+        }
+    }
+
+    void FirstPersonUpdate()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRot -= mouseY;
+        xRot = Mathf.Clamp(xRot, -80f, 80f);
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+
+        playerTransform.Rotate(Vector3.up * mouseX);
+    }
+
+    void ThirdPersonUpdate()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public CharacterController playerController;
+
+    public bool firstPerson = true;
+
+    public float speed = 6f;
+
+    private void Update()
+    {
+        if (Input.GetKey("f1"))
+        {
+            firstPerson = !firstPerson;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 move = Vector3.zero;
+        switch (firstPerson)
+        {
+            case true:
+                move = transform.right * x + transform.forward * z;
+                FirstPersonUpdate(move);
+                break;
+            case false:
+                move = new Vector3(x, 0f, z);
+                ThirdPersonUpdate(move);
+                break;
+        }
+    }
+
+    void FirstPersonUpdate(Vector3 moveVector)
+    {
+        playerController.Move(moveVector * speed * Time.deltaTime);
+    }
+
+    void ThirdPersonUpdate(Vector3 moveVector)
+    {
+
+    }
+}
